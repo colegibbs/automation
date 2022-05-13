32cbebe33e9cac02633bc6e8710686257c9f7436
Cole Gibbs <colegibbs0@gmail.com>
2022-05-12 17:51:55 -0700

    email function and docs

---
+++ README.md
@@ -0,0 +1,17 @@
+# LAB - Class x9
+
+## Project: Automation
+
+## Author: Cole Gibbs
+
+## Links and Resources
+
+- [Regex101](https://regex101.com/)
+- [Geeks for Geeks sets](https://www.geeksforgeeks.org/python-ways-to-remove-duplicates-from-list/)
+- [Geeks for Geeks map](https://www.geeksforgeeks.org/python-map-function/)
+
+## How to initialize/run your application (where applicable)
+
+1. call one of the find functions with the txt file in string form as a parameter
+2. run `python -m automation.automation` in the command line
+3. examine the `emails.txt` and `phone_numbers.txt` that are the outputs
--- automation/automation.py
+++ automation/automation.py
@@ -9,9 +9,8 @@ def find_phones(file):
   unformated_phones = [''.join(phone) for phone in unformated_phones]
   formated_phones = list(map(format_phone, unformated_phones))
   formated_phones = set(formated_phones)
-  print(formated_phones)
 
-  with open("assets/extracted-phones.txt", "w") as f:
+  with open("assets/phone_numbers.txt", "w") as f:
     for phone in formated_phones:
       f.write(f"{phone}\n")
 
@@ -24,4 +23,23 @@ def format_phone(phone):
   else:
     return phone
 
-find_phones("assets/potential-contacts.txt")
+def find_emails(file):
+  with open(file) as f:
+    file_text = f.read()
+  
+  pattern = r"([\w\d-]*@[\w\d-]*\.)(com|org|net|gov|edu)"
+  emails = re.findall(pattern, file_text)
+  
+  formatted = [''.join(email) for email in emails]
+  formatted = set(formatted)
+  email_str = '\n'.join(formatted)
+  
+  with open("assets/emails.txt", 'w') as f:
+    f.write(email_str)
+
+
+def find_emails_phones(file):
+  find_emails(file)
+  find_phones(file)
+
+find_emails_phones("assets/potential-contacts.txt")
